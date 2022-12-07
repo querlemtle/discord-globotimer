b2b0e648d65cffb7c57042e09ca2a98fd6dfb006
querlemtle <72937427+querlemtle@users.noreply.github.com>
2022-12-07 16:43:39 +0800

    feat: input date parse

--- commands/time.js
+++ commands/time.js
@@ -3,8 +3,17 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("time")
-    .setDescription("Replies corresponding local time"),
+    .setDescription("Replies corresponding local time")
+    .addStringOption(option =>
+      option.setName("date")
+        .setDescription("The input date that needs to be converted")),
   async execute(interaction) {
-    await interaction.reply(`Corresponding local time is: <t:${Math.floor(Date.now() / 1000)}>`);
+    const inputDate = interaction.options.getString("date");
+    const parsedDate = Date.parse(inputDate);
+    if (Number.isNaN(parsedDate)) {
+      await interaction.reply("Invalid date. Please check your input and try again.");
+    } else {
+      await interaction.reply(`Corresponding local time is: <t:${Math.floor(parsedDate / 1000)}>`);
+    }
   },
 };
